4c8b58cc5013c820a8584e3cd5cce782ce4ca74a
PauMartiNebot <paumtn00@gmail.com>
2025-05-09 09:53:12 +0200

    Arreglar redirect al crear un usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,10 +96,9 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
