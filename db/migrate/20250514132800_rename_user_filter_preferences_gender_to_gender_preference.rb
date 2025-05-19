dd1bd9ec9de09376b111c8ed8d0749e7f8e99ab6
PauMartiNebot <paumtn00@gmail.com>
2025-05-19 10:19:51 +0200

    Cambios migracion

--- db/migrate/20250514132800_rename_user_filter_preferences_gender_to_gender_preference.rb
+++ db/migrate/20250514132800_rename_user_filter_preferences_gender_to_gender_preference.rb
@@ -1,5 +1,5 @@
 class RenameUserFilterPreferencesGenderToGenderPreference < ActiveRecord::Migration[6.0]
   def change
-    rename_column :user_filter_preferences, :gender, :gender_preferences
+    rename_column :user_filter_preferences, :gender_preference, :gender_preferences
   end
 end
