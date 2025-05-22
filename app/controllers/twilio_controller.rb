009a83336c7611d0d90c5147ff70b02960b90dc5
PauMartiNebot <paumtn00@gmail.com>
2025-05-22 09:12:47 +0200

    Cambio controlador de twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -137,53 +137,53 @@ class TwilioController < ApplicationController
 	# Crea una conversación entre dos usuarios y los añade a los dos.
 	def create_conversation(user_id1, user_id2)
 
-		# set_account
+		 set_account
 
-		# unique_name = user_id1.to_s+'@'+user_id2.to_s
+		 unique_name = user_id1.to_s+'@'+user_id2.to_s
 
-		# begin # La conversacion existe.
+		 begin # La conversacion existe.
 
-		#    conversation = @client.conversations
-    #                   .conversations(unique_name)
-    #                   .fetch
+		    conversation = @client.conversations
+                       .conversations(unique_name)
+                       .fetch
 
-		# rescue # Conversacion no encontrada, creamos. 
+		 rescue # Conversacion no encontrada, creamos. 
 
-		#  conversation = @client.conversations
-		#   .conversations
-		#   .create(
-		#      unique_name: unique_name
-		#    )
+		  conversation = @client.conversations
+		   .conversations
+		   .create(
+		      unique_name: unique_name
+		    )
 
 
-		# end
+		 end
 
-		# begin
+		 begin
 		# # Agregamos los dos usuarios a la conversación
-		# @client.conversations
-    #                  .conversations(conversation.sid)
-    #                  .participants
-    #                  .create(
-    #                     identity: user_id1,
-    #                   )
-    #   rescue
-    #   	logger.info "Error adding participant 1 "+user_id1.inspect
-    #   end
+		 @client.conversations
+                      .conversations(conversation.sid)
+                      .participants
+                      .create(
+                         identity: user_id1,
+                       )
+       rescue
+       	logger.info "Error adding participant 1 "+user_id1.inspect
+       end
 
 
-    #   begin               
-		# @client.conversations
-    #                  .conversations(conversation.sid)
-    #                  .participants
-    #                  .create(
-    #                     identity: user_id2,
-    #                   )
-    #   rescue
-    #   	logger.info "Error adding participant 2 "+user_id2.inspect
-    #   end
+       begin               
+		 @client.conversations
+                      .conversations(conversation.sid)
+                      .participants
+                      .create(
+                         identity: user_id2,
+                       )
+       rescue
+       	logger.info "Error adding participant 2 "+user_id2.inspect
+       end
 
 
-		# return conversation.sid
+		 return conversation.sid
 
 	end
 
@@ -203,14 +203,14 @@ class TwilioController < ApplicationController
 
 	# Añade un mensaje a una conversación. Se usa en el momento del match para mandar un primer mensajito.
 	def send_message_to_conversation(sid, user_id, message)
-		# set_account
+		 set_account
 
-		# message = @client.conversations
-    #              .conversations(sid)
-    #              .messages
-    #              .create(author: user_id, body: message)
+		 message = @client.conversations
+                  .conversations(sid)
+                  .messages
+                  .create(author: user_id, body: message)
 
-    #     return message
+         return message
 
 	end
 
@@ -253,7 +253,7 @@ class TwilioController < ApplicationController
 
 		send_message_to_conversation(conversation_sid, 606, message)
 
-		#redirect_to show_user_path(id: umr.target_user), notice: 'Match generado con éxito.'
+		redirect_to show_user_path(id: umr.target_user), notice: 'Match generado con éxito.'
 
 
 	end
@@ -278,14 +278,10 @@ class TwilioController < ApplicationController
 
       @client = Twilio::REST::Client.new(@account_sid, auth_token)
 
-			puts "--------------------"
-			puts  ENV['TWILIO_SERVICE_SID']
-			puts "##################"
-
-		#configuration = @client.conversations
-                       #.services(ENV['TWILIO_SERVICE_SID'])
-                       #.configuration
-                       #.update(reachability_enabled: true)
+		configuration = @client.conversations
+                       .services(ENV['TWILIO_SERVICE_SID'])
+                       .configuration
+                       .update(reachability_enabled: true)
 		
 
 	end
