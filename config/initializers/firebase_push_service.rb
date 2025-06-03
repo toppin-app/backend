a7a927f39fa05f887c457be356fb947474dc14f4
PauMartiNebot <paumtn00@gmail.com>
2025-06-03 13:46:45 +0200

    Cambios push

--- config/initializers/firebase_push_service.rb
+++ config/initializers/firebase_push_service.rb
@@ -14,29 +14,18 @@ class FirebasePushService
     @access_token = authorizer.access_token
   end
 
-  def send_notification(token:, title:, body:, data: {}, sound: "default")
-        payload = {
-          message: {
-            token: token,
-            notification: {
-              title: title,
-              body: body
-            },
-            data: data.transform_keys(&:to_s), # Asegúrate de que las claves sean strings
-            android: {
-              notification: {
-                sound: sound
-              }
-            },
-            apns: {
-              payload: {
-                aps: {
-                  sound: sound
-                }
-              }
-            }
-          }
+    def send_notification(token:, title:, body:, data: {}, sound:)
+      payload = {
+        message: {
+          token: token,
+          notification: {
+            title: title,
+            body: body,
+            sound: sound
+          },
+          data: data
         }
+      }
 
     headers = {
       "Authorization" => "Bearer #{@access_token}",
