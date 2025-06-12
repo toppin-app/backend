e09e4d90ae818d80b8453d0b2034fb426ee40568
PauMartiNebot <paumtn00@gmail.com>
2025-06-12 10:02:38 +0200

    Mapeo notification localizer

--- config/initializers/notification_localizer.rb
+++ config/initializers/notification_localizer.rb
@@ -1,6 +1,12 @@
 class NotificationLocalizer
   def self.for(user:, type:, params: {})
-    I18n.with_locale(user.language.presence || I18n.default_locale) do
+    locale = case user.language
+             when 0, '0', :es, 'es', 'ES' then :es
+             when 1, '1', :en, 'en', 'EN' then :en
+             else I18n.default_locale
+             end
+
+    I18n.with_locale(locale) do
       {
         title: I18n.t("notifications.#{type}.title", **params),
         body: I18n.t("notifications.#{type}.body", **params)
