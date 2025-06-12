11014d37a991991919b320173560b3c03813fe54
PauMartiNebot <paumtn00@gmail.com>
2025-06-12 09:57:38 +0200

    Notis prueba

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -218,12 +218,13 @@ class UsersController < ApplicationController
 
     target_user = User.find(umr.target_user)
     devices = Device.where(user_id: target_user.id)
+    notification = NotificationLocalizer.for(user: umr.user, type: :like)
      devices.each do |device|
        if device.token.present?
         FirebasePushService.new.send_notification(
            token: device.token,
-           title: "¡Wow! Tienes nuevos admiradores :-)",
-           body: "Has recibido nuevos me gusta",
+           title: notification[:title],
+           body: notification[:body],
            data: { action: "like", user_id: umr.user_id.to_s }
          )
        end
