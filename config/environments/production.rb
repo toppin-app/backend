4e6138571e5f1153f50a89528ecf6bc9114ab085
PauMartiNebot <paumtn00@gmail.com>
2025-06-19 11:27:32 +0200

    cambios production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://web-backend-ruby.uao3jo.easypanel.host/cable'
   config.action_cable.allowed_request_origins = [
-    'https://web-backend-ruby.uao3jo.easypanel.host'
+    '*'
   ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
