d193d0f541dd6882a8f8bcded0dfb03c50d53b25
PauMartiNebot <paumtn00@gmail.com>
2025-06-19 15:35:08 +0200

    cambio consulta bbdd

--- app/models/user_match_request.rb
+++ app/models/user_match_request.rb
@@ -39,9 +39,12 @@ class UserMatchRequest < ApplicationRecord
 
   end
   # Comprueba si existe un match confirmado entre dos usuarios
-  def self.match_confirmed_between?(user1, user2)
-  where(user_id: user1.id, target_user: user2.id, is_match: 1).exists? &&
-  where(user_id: user2.id, target_user: user1.id, is_match: 1).exists?
+def self.match_confirmed_between?(user_a, user_b)
+  where(
+    is_match: true,
+    user_id: [user_a, user_b],
+    target_user: [user_a, user_b]
+  ).exists?
 end
   
 
