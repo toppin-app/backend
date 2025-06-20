01968956829552d88964977cd837dcc7ae47c19e
PauMartiNebot <paumtn00@gmail.com>
2025-06-20 16:06:06 +0200

    cambios connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,15 +10,16 @@ module ApplicationCable
 
     def find_verified_user
       token = request.params[:token]
-      
+
       if token.present?
         begin
           # Ajusta aquí la clave según cómo tengas configurado devise-jwt o tu JWT
+          jwt = token.start_with?('Bearer ') ? token.split(' ', 2).last : token
           secret_key = Rails.application.credentials.devise[:jwt_secret_key]
           decoded_token = JWT.decode(token, secret_key, true, algorithm: 'HS256')
           user_id = decoded_token[0]['sub'] # asumiendo que el user_id está en 'sub'
           return User.find(user_id)
-        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+          rescue JWT::DecodeError, ActiveRecord::RecordNotFound
           # Token inválido o usuario no encontrado
           reject_unauthorized_connection
         end
