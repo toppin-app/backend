b61c07a262cb8709373996192a813fa914ed2f35
PauMartiNebot <paumtn00@gmail.com>
2025-07-02 17:56:50 +0200

    alive_channel

--- app/channels/alive_channel.rb
+++ app/channels/alive_channel.rb
@@ -1,6 +1,6 @@
 class AliveChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "alive_channel"
+    stream_for current_user
     redis.sadd("online_users", current_user.id)
     Rails.logger.info("[AliveChannel] Usuario conectado: #{current_user.id}")
     alive_channel
@@ -9,32 +9,29 @@ class AliveChannel < ApplicationCable::Channel
   def unsubscribed
     redis.srem("online_users", current_user.id)
     Rails.logger.info("[AliveChannel] Usuario desconectado: #{current_user.id}")
-    alive_channel # Notifica a todos los usuarios
+    alive_channel
   end
 
-  # Método para enviar la lista de usuarios conectados a todos los clientes
+  # Notifica a cada usuario su propia lista de matches y su estado online
   def alive_channel
     Rails.logger.info("[AliveChannel] Enviando lista de matches conectados")
     user_ids = redis.smembers("online_users").map(&:to_i)
-
-    all_matches = []
 
     User.find_each do |user|
       match_requests = UserMatchRequest.where("(user_id = :id OR target_user = :id) AND is_match = true", id: user.id)
       match_user_ids = match_requests.map { |mr| mr.user_id == user.id ? mr.target_user : mr.user_id }
       matches = User.where(id: match_user_ids)
 
-      matches.each do |match|
-        all_matches << {
-          user_id: user.id,
-          match_id: match.id,
-          match_name: match.name,
+      matches_data = matches.map do |match|
+        {
+          id: match.id,
+          name: match.name,
           online: user_ids.include?(match.id)
         }
       end
-    end
 
-    ActionCable.server.broadcast("alive_channel", { type: "all_matches_status", matches: all_matches })
+      AliveChannel.broadcast_to(user, { type: "online_matches", matches: matches_data })
+    end
   end
 
   private
