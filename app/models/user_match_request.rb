3d92e8b7dc05615f7677c6ff9f4f36bc69b61501
PauMartiNebot <paumtn00@gmail.com>
2025-07-03 13:21:32 +0200

    user_match_request sintax error

--- app/models/user_match_request.rb
+++ app/models/user_match_request.rb
@@ -2,28 +2,27 @@ class UserMatchRequest < ApplicationRecord
   belongs_to :user
   has_many :user_media, through: :user
 
-  scope :rejected, -> (user_id)  { where("(user_id = ? or target_user = ?) and is_rejected is true", user_id, user_id) }
+  scope :rejected, -> (user_id) {
+    where("(user_id = ? or target_user = ?) and is_rejected is true", user_id, user_id)
+  }
 
   def target
     us = User.find_by(id: target_user)
     if !us
       self.destroy
-      return 
+      return
     else
       return us
     end
   end
 
-
-
   def self.ranking
-      UserMatchRequest.all.each do |umr|
-          umr.user_ranking = umr.user.ranking
-          umr.target_user_ranking = umr.target.ranking
-          umr.save
-      end
+    UserMatchRequest.all.each do |umr|
+      umr.user_ranking = umr.user.ranking
+      umr.target_user_ranking = umr.target.ranking
+      umr.save
+    end
   end
-
 
   # Busca un match_request entre dos usuarios.
   def self.match_between(user1, user2)
@@ -36,15 +35,12 @@ class UserMatchRequest < ApplicationRecord
     return inverse
   end
 
-  end
   # Comprueba si existe un match confirmado entre dos usuarios
-    def self.match_confirmed_between?(user_a, user_b)
-      where(
-        is_match: true,
-        user_id: [user_a, user_b],
-        target_user: [user_a, user_b]
-      ).exists?
-    end
-  
-
+  def self.match_confirmed_between?(user_a, user_b)
+    where(
+      is_match: true,
+      user_id: [user_a, user_b],
+      target_user: [user_a, user_b]
+    ).exists?
+  end
 end
