fdf39351bafcd52bc23061a7d0e9822360229149
PauMartiNebot <paumtn00@gmail.com>
2025-07-08 11:15:44 +0200

    nueva migracion para pelis y series

---
+++ db/migrate/20250708103945_create_user_watchlist.rb
@@ -0,0 +1,12 @@
+class CreateUserWatchlists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_watchlists do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :media_id, null: false # ID en TMDB
+      t.string :media_type, null: false # 'movie' o 'tv'
+
+      t.timestamps
+    end
+    add_index :user_watchlists, [:user_id, :media_id, :media_type], unique: true
+  end
+end
