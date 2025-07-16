3022f0098f29e51adb9118e0d38d760ac48c2e67
PauMartiNebot <paumtn00@gmail.com>
2025-07-16 09:29:23 +0200

    cambios firebase

--- config/initializers/firebase_push_service.rb
+++ config/initializers/firebase_push_service.rb
@@ -1,9 +1,7 @@
 require 'googleauth'
 require 'httparty'
-
 class FirebasePushService
   FCM_ENDPOINT = "https://fcm.googleapis.com/v1/projects/toppin-456209/messages:send"
-
   def initialize
     scope = ['https://www.googleapis.com/auth/firebase.messaging']
     authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
@@ -14,34 +12,36 @@ class FirebasePushService
     @access_token = authorizer.access_token
   end
 
-  def send_notification(token:, title:, body:, data: {}, sound: "default")
+  def send_notification(token:, title:, body:, data: {}, sound: "default", category: nil)
+  def send_notification(token:, title:, body:, data: {}, sound: "default", category: nil, channel_id:)
     payload = {
       message: {
         token: token,
+        data: data.transform_keys(&:to_s), # Custom data
         notification: {
           title: title,
           body: body
         },
-        data: data.transform_keys(&:to_s), # Los datos personalizados son Strings en las claves
         android: {
           notification: {
-            sound: sound
+            sound: sound,
+            channel_id: "sms-channel" # Asegúrate de que esta channelId esté registrada en tu app Android
           }
         },
         apns: {
-          # Este es el payload de APNS correcto, donde 'aps' y los datos personalizados van dentro de 'payload'
           payload: {
             aps: {
               alert: {
                 title: title,
                 body: body
               },
-              sound: sound
+              sound: sound,
+              category: category || "default" # Notifee lo puede usar para mostrar acciones personalizadas
             }
-            # Si necesitas datos personalizados también en el payload de APNS (además de en el 'data' global de FCM),
-            # los agregarías aquí, por ejemplo:
-            # "custom_action": data[:action],
-            # "custom_user_id": data[:user_id]
+          },
+          headers: {
+            "apns-priority": "10", # Alta prioridad, importante para background
+            "apns-push-type": "alert" # Necesario en iOS 13+ para notificaciones visibles
           }
         }
       }
@@ -50,14 +50,11 @@ class FirebasePushService
       "Authorization" => "Bearer #{@access_token}",
       "Content-Type" => "application/json"
     }
-
     response = HTTParty.post(FCM_ENDPOINT, headers: headers, body: payload.to_json)
-
-    # Mejorar el log para errores
     if response.code != 200
-      Rails.logger.error "FCM Error (iOS): Code=#{response.code}, Body=#{response.body}, Payload=#{payload.to_json}"
+      Rails.logger.error "❌ FCM Error: Code=#{response.code}, Body=#{response.body}, Payload=#{payload.to_json}"
     else
-      Rails.logger.info "FCM Success (iOS): Code=#{response.code}, Body=#{response.body}"
+      Rails.logger.info "✅ FCM Success: Code=#{response.code}, Body=#{response.body}"
     end
     response
   end
