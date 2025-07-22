25a9e3842991a46f82a893fee93eac4a899e8ead
PauMartiNebot <paumtn00@gmail.com>
2025-07-22 08:58:25 +0200

    cambios development para dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts << "web-backend-ruby.uao3jo.easypanel.host"
+  config.hosts << "web-backend-ruby-dev.uao3jo.easypanel.host"
   # Do not eager load code on boot.
   config.eager_load = false
 
