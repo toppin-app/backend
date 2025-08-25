b89ae3154b0d7a6730030a505b0c38a06a7fc43e
PauMartiNebot <paumtn00@gmail.com>
2025-08-25 10:19:48 +0200

    envio gender_preferences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,8 @@ class UsersController < ApplicationController
             :user_info_item_values,
             :user_main_interests,
             :tmdb_user_data,
-            :tmdb_user_series_data# <-- Añade esto para incluir los datos de TMDB
+            :tmdb_user_series_data,
+            :gender_preferences
           ]
         )
       end
@@ -90,7 +91,8 @@ class UsersController < ApplicationController
       :user_info_item_values,
       :user_main_interests,
       :tmdb_user_data,
-      :tmdb_user_series_data # <-- añade esto
+      :tmdb_user_series_data,
+      :gender_preferences
     ]
   )
   end
