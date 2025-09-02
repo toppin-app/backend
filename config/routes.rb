4eda26e5fcd65ff3517b71665de53b7bb75586a3
PauMartiNebot <paumtn00@gmail.com>
2025-09-02 10:56:06 +0200

    cambio endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -207,7 +207,7 @@ Rails.application.routes.draw do
   # Rutas Stripe
   post '/stripe/create_payment_session', to: 'stripe#create_payment_session'
   post '/stripe/ensure_customer', to: 'stripe#ensure_customer'
-  get '/stripe/publishable_key', to: 'stripe_controller#publishable_key'
+  get '/stripe/publishable_key', to: 'stripe#publishable_key'
   # Rutas para token spoti
   get '/spotify/token', to: 'spotify#token'
 
