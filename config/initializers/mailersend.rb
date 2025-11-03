a17868c3f4caa4ff54c412fb25be561d0c2cd1b6
Pau Nebot <paumtn00@gmail.com>
2025-11-03 10:21:05 +0100

    mas cambios para los emilios en el panel

--- config/initializers/mailersend.rb
+++ config/initializers/mailersend.rb
@@ -4,6 +4,9 @@
 # Para usar MailerSend, necesitas un API Token desde: https://www.mailersend.com/
 # Settings → API Tokens
 
+# Cargar el delivery method personalizado
+require_relative '../../lib/mailersend_delivery_method'
+
 # La gema mailersend-ruby se configura automáticamente desde las variables de entorno
 # No requiere configuración adicional aquí
 
