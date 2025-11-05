ce6c089caf65feec54158dabaa4617f2c85853f2
PauMartiNebot <paumtn00@gmail.com>
2025-11-05 12:25:06 +0100

    cambio logo

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -7,19 +7,8 @@ class WelcomeMailer < ApplicationMailer
     
     # Cargar el logo desde public
     logo_path = Rails.root.join('public', 'logo-devise.png')
-    
-    if File.exist?(logo_path)
-      begin
-        logo_content = File.binread(logo_path)
-        @logo_base64 = Base64.strict_encode64(logo_content)
-      rescue => e
-        Rails.logger.error "Error leyendo logo: #{e.message}"
-        @logo_base64 = nil
-      end
-    else
-      Rails.logger.warn "Logo no encontrado en: #{logo_path}"
-      @logo_base64 = nil
-    end
+    logo_content = File.binread(logo_path)
+    @logo_base64 = Base64.strict_encode64(logo_content)
     
     mail(
       to: @user.email,
