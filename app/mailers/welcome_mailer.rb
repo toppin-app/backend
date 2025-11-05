7636b0435a8a04668bb72bcc112a8e1d31325ba5
PauMartiNebot <paumtn00@gmail.com>
2025-11-05 11:32:34 +0100

    cambio logo emilio

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,6 +3,12 @@ class WelcomeMailer < ApplicationMailer
     @user = user
     @app_url = ENV['MAILJET_DEFAULT_URL_HOST'] || 'toppin.es'
     
+    # Adjuntar el logo como inline attachment para que se vea en el email
+    logo_path = Rails.root.join('public', 'logo-devise.png')
+    if File.exist?(logo_path)
+      attachments.inline['logo-toppin.png'] = File.read(logo_path)
+    end
+    
     mail(
       to: @user.email,
       subject: '¡Bienvenido a Toppin! 🍩'
