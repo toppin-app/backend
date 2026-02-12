ad158ba30ed71e4017149f57eda926b7b60f1160
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2026-02-12 14:35:45 +0100

    Fix eliminación: agrega dependent: :destroy y función JS custom para confirmación

--- app/models/info_item_category.rb
+++ app/models/info_item_category.rb
@@ -1,5 +1,5 @@
 class InfoItemCategory < ApplicationRecord
-  has_many :info_item_values
+  has_many :info_item_values, dependent: :destroy
 
   def info_items_with_values
     self.info_item_values
